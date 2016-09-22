37246e89f6e8b69b3d1d4286a7503c47361abb1a
Anders Åslund <anders.aslund@me.com>
2016-09-22 17:54:56 +0200

    New logo

--- client/tile_anim.js
+++ client/tile_anim.js
@@ -60,7 +60,7 @@ setup = function() {
         }
     }
 
-    var teknoir = snap.text(size.x/2, size.y/2, "teknoır");
+    var teknoir = snap.text(size.x/2, size.y/2, "tekn⚈ır");
     teknoir.attr({
         "text-anchor": "middle",
         "alignment-baseline": "middle",
